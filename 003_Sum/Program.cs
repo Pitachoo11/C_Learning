19ea92d3bb348a38dd672cffa5d4583293d25c32
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-18 17:28:37 +0300

    003_Sum_Random

--- 003_Sum/Program.cs
+++ 003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int NumberA = 3;
-int NumberB = 5;
+﻿int NumberA = new Random(). Next(1,10); //Random number from 1-9 range
+Console.WriteLine(NumberA);
+int NumberB = new Random(). Next(1,10); //Random number from 1-9 range
+Console.WriteLine(NumberB);
 Console.WriteLine(NumberA + NumberB);
