fe8621ee4ded5cf0f03a794c899aa4b7381dd151
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-18 17:56:10 +0300

    006_WeightComp

---
+++ 006_WeightComp/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.Write("Max value:");
+Console.WriteLine(max);
