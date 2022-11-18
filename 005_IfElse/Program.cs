586d52e37a45c99e6ffc14d36b489c8b86b355d5
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-18 17:41:30 +0300

    005_IfElse

---
+++ 005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter your Name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "sasha")
+{
+  Console.WriteLine("Great, hello Sasha");  
+}
+else
+{
+   Console.Write("Hello, "); 
+   Console.WriteLine(username);  
+}
+
