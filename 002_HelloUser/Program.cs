338fc2ffd7a003d30b1d5d491432f7b0552501f7
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-18 17:01:44 +0300

    002_HelloUser

---
+++ 002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your Name");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
