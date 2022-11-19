71a0b390c0d109becdf94c43562c5dff521e490b
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-19 12:53:02 +0300

    008_SqCheck

---
+++ 008_SqCheck/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine());
+
+int square = number2 * number2;
+
+if (square == number1)
+{
+    Console.WriteLine("да, первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("нет, первое число не является квадратом второго");
+}
+
+
+
+
+
