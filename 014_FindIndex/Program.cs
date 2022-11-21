70aecbd03b313be30af5bab2821b2d07b15a1482
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-21 12:52:03 +0300

    014_FindIndex

---
+++ 014_FindIndex/Program.cs
@@ -0,0 +1,19 @@
+﻿// Поиск индекса элемента массива со значением find (например 4)
+
+int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+} 
