fb84a536280a6f79bbe0e3c58bedc4201b619d8d
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-21 21:28:01 +0300

    020_CalculateN

---
+++ 020_CalculateN/Program.cs
@@ -0,0 +1,11 @@
+﻿//дано натуральное n. вычислить: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n.
+
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+double s = 0;
+
+for (double i = 1; i <= n; i++)
+{
+   s=s+(1/i);
+}
+Console.WriteLine(s);
