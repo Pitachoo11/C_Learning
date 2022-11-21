d4ae3ab410e96ba2815c7195b58c1453b9041d53
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-21 20:46:14 +0300

    016_3DigitsRemove

---
+++ 016_3DigitsRemove2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int x = new Random().Next(100,999);
+System.Console.WriteLine(x);
+int position = 2; //какую цифру удаляем
+int m = 1;
+while (position > 1)
+{
+     m *= 10;
+     position--;
+}
+int t = (x % m) + (x / (m * 10)) * m;
+Console.WriteLine(t);
