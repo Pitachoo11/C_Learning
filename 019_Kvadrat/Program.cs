d3d3cc79aa6ac6478b43c1363bcc329914b5ecc6
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-21 21:12:06 +0300

    019_Kvadrat

---
+++ 019_Kvadrat/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number2*number2 == number1 ||  number1*number1 == number2 ? "Является квадратом" : "Не является квадратом");
