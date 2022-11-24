61c16bddc3ac826d9874dc4043c102a14b255d1d
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-24 14:17:55 +0300

    030_Factorial

---
+++ 030_Factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вычисление факториала числа N через рекурсию
+
+int Factorial(int N)
+{
+    if (N == 1)
+    {
+       return 1; 
+    }
+    else
+    {
+        return N * Factorial(N-1);
+    }
+}
+
+System.Console.WriteLine(Factorial(5));
