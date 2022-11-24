f12706167adab448cf56b0bdab0fb4886ad4246d
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-24 14:03:37 +0300

    028_2dArrayNum

---
+++ 028_2dArrayNum/Program.cs
@@ -0,0 +1,32 @@
+﻿
+void FillArray(int[,] numarray)
+{
+    for (int rows = 0; rows < numarray.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < numarray.GetLength(1); columns++)
+        {
+            numarray[rows, columns] = new Random().Next(1, 10);
+        }
+    }
+} 
+
+void PrintArray(int[,] numarray)
+{
+    for (int rows = 0; rows < numarray.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < numarray.GetLength(1); columns++)
+        {
+            System.Console.Write($"{numarray[rows, columns]} ");
+        }
+    System.Console.WriteLine();
+    }
+} 
+
+int[,] matrix = new int[3,4];
+
+PrintArray(matrix);
+
+FillArray(matrix);
+System.Console.WriteLine();
+
+PrintArray(matrix);
